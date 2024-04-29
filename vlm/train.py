ddb426f5648480c72efb15ddb125bb3656f603ad
Alexander Moore <31463017+alexander-moore@users.noreply.github.com>
2024-04-29 09:44:14 -0700

    Update train.py

--- vlm/train.py
+++ vlm/train.py
@@ -2,18 +2,35 @@
 Train a vlm
 """
 
+device = 7
+# def batch_to_device(batch):
+#     for key, value in batch.items():
+#         try:
+#             batch[key] = value.to(device)
+#             print(key, value)
+#         except:
+#             pass
 
-def train_model():
-    
+def train_model(model, n_epochs):
+    model = model.to(device)
     model.train()
     optimizer.train()
     
     for _ in range(n_epochs):
-        for batch in train_dataloader:
+        losses = []
+        for bi, batch in enumerate(train_dataloader):
             optimizer.zero_grad()
+            
+            #batch = batch_to_device(batch)
+            batch['image'] = batch['image'].to(device)
+            
             logits, loss = model.forward(batch)
             loss.backward()
             optimizer.step()
+            
+            losses.append(loss.data.item())
+            if bi % 100 == 0:
+                print(sum(losses) / len(losses))
             
         val_metrics = val_step()
         print(val_metrics)
@@ -37,7 +54,7 @@ if __name__ == '__main__':
     # Model
     from vlm import build_vlm
 
-    model = build_vlm()
+    model = build_vlm().to(device)
     print(model)
 
     # Optimizer
@@ -50,11 +67,13 @@ if __name__ == '__main__':
 
     #val_dataset = get_pokemon_dataset()
     #val_dataset = get_coco_dataset()
-    train_dataset = get_coco_dataset()
-    
+    train_dataset = get_coco_dataset(mode='train')
     train_dataloader = DataLoader(train_dataset, batch_size = 1)
+    
+    val_dataset = get_coco_dataset(mode = 'val')
+    val_dataloader = DataLoader(val_dataset, batch_size = 1)
 
     print(train_dataset, val_dataset)
     n_epochs = 2
     
-    train_model()
+    train_model(model, n_epochs)
