e387701c9ca479cb4f17099314c8d405fd3f1a93
Alexander Moore <31463017+alexander-moore@users.noreply.github.com>
2024-07-02 09:40:43 -0700

    Update projection.py

--- vlm/modeling/projection.py
+++ vlm/modeling/projection.py
@@ -1,52 +1,82 @@
+import torch.nn as nn
+
+
 """
-Language Model Base
--------------------
-
-We need a language model to serve as the initial point. This language model will be frozen during training and used to decode custom language-vision inputs
+Currently just using simple linear projection.
+Literature often uses tiny attention models like xformer (?) mini-gpt etc to get output tokens and then project to new LLM dim
 """
-import huggingface_hub
-import transformers
-#from transformers import PhiForCausalLM, PhiForCausalLM
-from transformers import AutoTokenizer, AutoModelForCausalLM
 
+def get_image_tokenizer(projector_str, insize, outsize):
+    if 'qformer' in projector_str:
+        #config = BlipConfig(in_dim = insize,
+        #                    out_dim = outsize)
+        #return QFormer(config)
+        pass
+    else:
+        return ImageTokenizer(insize, outsize)
+    
+def get_in_size(image_encoder):
+    return image_encoder.config.hidden_size
 
-def get_llm(llm_name, peft = False):
-    """
+def get_out_size(lang_tokenizer, llm):
+    input_ids = lang_tokenizer.encode('hi', add_special_tokens=True, return_tensors="pt")
+    vec = llm.get_input_embeddings()(input_ids)
+    embedded_tokens_size = vec.size()[-1]
+    return embedded_tokens_size
+
+class ImageTokenizer(nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super(ImageTokenizer, self).__init__()
+        self.fc1 = nn.Linear(in_dim, out_dim)
+        self.fc2 = nn.Linear(out_dim, out_dim)
+        self.activ = nn.GELU()
     
-    """
-    tokenizer = AutoTokenizer.from_pretrained(llm_name, trust_remote_code = True)
-    model = AutoModelForCausalLM.from_pretrained(llm_name, trust_remote_code = True)
+    def forward(self, x):
+        """
+        Forward maps vision_encoder outputs to llm_token input size
+        (bs, seq_length, in_size) -> (bs, seq_length, out_size)
+        """
+        return self.fc2(self.activ(self.fc1(x)))
     
-    # Freeze LLM
-    for name, param in model.named_parameters():
-        param.requires_grad = False
-            
-    # Add PEFT?
-    if peft is True:
-        # Optional peft component for lora
-        from peft import LoraConfig, PeftModel, get_peft_model
+# class BlipConfig():
+#     """
+#     Build the config to pass to Blip2Qformer
+#     """
+#     def __init__(self, in_dim, out_dim):
+#         self.hidden_size = in_dim
+#         self.num_attention_heads = 8
         
-        peft_config = LoraConfig(
-        task_type=None, inference_mode=False, r=64, 
-        lora_alpha=32, lora_dropout=0.1, 
-        target_modules=['gate_up_proj', 'down_proj']
-        )
-        
-        model = get_peft_model(model, peft_config)
-        model.print_trainable_parameters()
 
+# def QFormer(config):
+#     from transformers import Blip2QFormerModel
+#     return Blip2QFormerModel()
 
-    return tokenizer, model
+def qformer():
+    import torch
+    from qformer import QFormer
 
-if __name__ == '__main__':
-    """
-    This function is used to test llm.py correctly builds a model
-    """
-    tokenizer, model = get_llm("microsoft/Phi-3-mini-4k-instruct")
+    # Create a random tensor of shape (1, 32, 512)
+    x = torch.randn(1, 32, 512)
+
+    # Create a random image tensor of shape (1, 3, 224, 224)
+    img = torch.randn(1, 3, 224, 224)
+
+    # Create an instance of the QFormer model with the following parameters:
+    # - input_size: 512
+    # - num_heads: 8
+    # - num_layers: 8
+    # - dropout: 0.1
+    # - num_classes: 2
+    # - num_patches: 2
+    qformer = QFormer(512, 8, 8, 0.1, 2, 2)
+
+    # Apply the QFormer model to the input tensors x and img
+    y = qformer(x, img)
     
-    prompt = "Hey, are you conscious? Can you talk to me?"
-    inputs = tokenizer(prompt, return_tensors="pt")
+    
+    # Print the shape of the output tensor y
+    print(y.shape)
+    
+    # Then I think we literally jam the QFORMER output into the LLM? 
+    # I think? lol. that ounds crazty
 
-    # Generate
-    generate_ids = model.generate(inputs.input_ids, max_length=30)
-    print(tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0])
